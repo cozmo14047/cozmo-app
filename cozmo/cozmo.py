363b23f74b98bb11743d1da6baa58fd80571cb13
cozmo14047 <134814825+cozmo14047@users.noreply.github.com>
2023-12-14 13:08:50 +1300

    Update cozmo.py
    Removed buttons and changed theme to sliders so you can adjust how far his head and lift move

--- cozmo/cozmo.py
+++ cozmo/cozmo.py
@@ -1,72 +1,80 @@
 import tkinter
+from tkinter import Tk, Scale, Label
+import tkinter as tk
 import pycozmo
 import time
 from PIL import ImageTk, Image
 logo_path = "logo.jpg"
 # Connect to Cozmo
+
+# Define minimum and maximum lift height
+MIN_LIFT_HEIGHT = pycozmo.robot.MIN_LIFT_HEIGHT.mm
+MAX_LIFT_HEIGHT = pycozmo.robot.MAX_LIFT_HEIGHT.mm
+
+def update_lift_height(new_height):
+    global current_lift_height
+    current_lift_height = new_height
+    cli.set_lift_height(current_lift_height)
+   
+    # Update label to show current lift height
+    
+
+root = tkinter.Tk()
+root.title("Cozmo")
+root.configure(bg="#5f5e5f")
+root.geometry("400x300")
+    
+image = Image.open(logo_path)
+resized_image = image.resize((550, 100))
+tk_image = ImageTk.PhotoImage(resized_image)
+logo_label = tkinter.Label(root, image=tk_image)
+logo_label.pack()
+
+
+
+lift_slider = Scale(root, from_=MIN_LIFT_HEIGHT, to_=MAX_LIFT_HEIGHT, orient="vertical",
+                    command=update_lift_height, bg="black",troughcolor="#000000",fg="#ffffff",highlightthickness=0)
+lift_slider.pack(side=tk.RIGHT, fill=tk.Y)
+lift_slider.pack()
+
 with pycozmo.connect() as cli:
     time.sleep(1)
     angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
     cli.set_head_angle(angle)
     time.sleep(1)
-      # Set volume to ~75%.
+    # Set initial lift height
+    current_lift_height = MIN_LIFT_HEIGHT
+    update_lift_height(current_lift_height)
     cli.set_volume(50000)
 
     # A 22 kHz, 16-bit, mono file is required.
     cli.play_audio("hello.wav")
     cli.wait_for(pycozmo.event.EvtAudioCompleted)
     
+    min_angle = pycozmo.robot.MIN_HEAD_ANGLE.radians
+    max_angle = pycozmo.robot.MAX_HEAD_ANGLE.radians
 
-    # Initialize Tkinter
-    root = tkinter.Tk()
-    root.title("Cozmo")
-    root.configure(bg="#5f5e5f")
-    root.geometry("400x300")
-    
-    image = Image.open(logo_path)
-    resized_image = image.resize((550, 100))
-    tk_image = ImageTk.PhotoImage(resized_image)
-    logo_label = tkinter.Label(root, image=tk_image)
-    logo_label.pack()
+    def update_angle_label():
+        
+       current_angle = cli.get_head_angle().radians
+       doubled_angle = 2 * current_angle
+       current_angle_label.config(text=f"Current Head Angle: {float(doubled_angle):.2f}")
 
-    # Label with message
-    message_label = tkinter.Label(
-        root, text="", fg="white", bg="#5f5e5f"
+    head_angle_slider = tk.Scale(
+        root,
+        from_=min_angle,
+        to=max_angle,
+        orient=tk.VERTICAL,
+        resolution=0.01,
+        command=lambda angle: (cli.set_head_angle(angle), update_angle_label()),
+         troughcolor="#000000",  # Sets background color of the slider
+         bg="#000000",  # Sets background color of the slide bar
+         fg="#ffffff",  # Sets the color of the slider knob and tick marks
+         highlightthickness=0,
     )
-    message_label.pack()
+    head_angle_slider.pack(side=tk.LEFT, fill=tk.Y)
 
-    # Button to raise head
-    raise_head_button = tkinter.Button(
-        root, text="Raise Head", command=lambda: cli.set_head_angle(angle + 0.4)
-    )
-    raise_head_button.pack()
+    head_angle_slider.pack()
 
-    raise_lift_button = tkinter.Button(
-        root, text="Raise lift", command=lambda: cli.set_lift_height(pycozmo.MAX_LIFT_HEIGHT.mm)
-    )
-    raise_lift_button.pack()
-
-
-
-    lower_lift_button = tkinter.Button(
-        root, text="lower lift", command=lambda: cli.set_lift_height(pycozmo.MIN_LIFT_HEIGHT.mm)
-    )
-    lower_lift_button.pack()
-    
-
-
-    # Button to lower head
-    lower_head_button = tkinter.Button(
-        root, text="Lower Head", command=lambda: cli.set_head_angle(angle - .8)
-    )
-    lower_head_button.pack()
-
-    # Calculate head angles:
-    angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
-    raise_angle = angle + 0.4  # 0.4 radians higher than neutral
-
-    # Start main loop
+    # Main loop
     root.mainloop()
-
-# Disconnect from Cozmo after tkinter closes
-cli.disconnect()
